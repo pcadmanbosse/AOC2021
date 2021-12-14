3850da9093d41c1e4e45247d425a1fb4cb53481e
pierre cadman bosse <pierre.cadman@karetis.com>
2021-12-14 12:13:04 +0100

    makeDay update

--- makeDay.sh
+++ makeDay.sh
@@ -1,10 +1,12 @@
 echo "Day number $1";
-echo "Making folder 6";
+echo "Making folder day$1";
 mkdir "day$1"
 echo "Making input & code files";
 curl  "https://adventofcode.com/2021/day/$1/input" -X GET -H "Cookie:session=$2" >> "day$1/input.txt";
 touch "day$1/day$1.js";
+echo "Adding a bit of boilerplate node"
 echo "const fs = require('fs');" >> "day$1/day$1.js";
 echo "const input = fs.readFileSync('input.txt').toString().split('\n');" >> "day$1/day$1.js";
 cd "day$1"
+echo "opening vscode"
 code -r .;
